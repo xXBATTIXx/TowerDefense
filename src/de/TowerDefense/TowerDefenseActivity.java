f9923e56dca27fa5ed2412d25c29cd33040c1114
Tobias <TSKalletal@t-online.de>
2012-08-08 18:48:03 +0200

    added start button

--- src/de/TowerDefense/TowerDefenseActivity.java
+++ src/de/TowerDefense/TowerDefenseActivity.java
@@ -2,12 +2,29 @@ package de.TowerDefense;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.Button;
 
-public class TowerDefenseActivity extends Activity {
+public class TowerDefenseActivity extends Activity implements OnClickListener {
     /** Called when the activity is first created. */
+	
+	private Button button_start;
+	
+	
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.main);
+        
+        button_start = (Button) findViewById(R.id.button_start);
+        button_start.setOnClickListener(this);
+        
+    }
+    
+    public void onClick(View v) {
+    	
+    	
+    	
     }
 }
