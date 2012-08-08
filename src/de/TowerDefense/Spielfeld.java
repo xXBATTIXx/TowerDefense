47af310308d97d487be72d3954b21749758a71a6
Tobias <TSKalletal@t-online.de>
2012-08-08 20:44:23 +0200

    rechteck

--- src/de/TowerDefense/Spielfeld.java
+++ src/de/TowerDefense/Spielfeld.java
@@ -1,8 +1,38 @@
 package de.TowerDefense;
 
-public class Spielfeld {
+import android.content.Context;
+import android.graphics.Canvas;
+import android.graphics.Color;
+import android.graphics.Paint;
+import android.view.View;
+
+public class Spielfeld extends View {
 	
 	private int x;
 	private int y;
+	
+	
+	
+	 public Spielfeld(Context context) {
+         super(context);
+     }
+ 
+     @Override
+     protected void onDraw(Canvas c){
+         super.onDraw(c);
+         
+         Paint paint = new Paint();
+		 paint.setStyle(Paint.Style.FILL);
+
+		 // make the entire canvas white
+		 paint.setColor(Color.WHITE);
+		 c.drawPaint(paint);
+		 
+		 paint.setColor(Color.BLUE);
+		 c.drawRect(50, 100, 200, 3, paint);
+         
+     }
+
+	
 
 }
--- src/de/TowerDefense/TowerDefenseActivity.java
+++ src/de/TowerDefense/TowerDefenseActivity.java
@@ -2,6 +2,8 @@ package de.TowerDefense;
 
 import android.app.Activity;
 import android.app.AlertDialog;
+import android.graphics.Color;
+import android.graphics.Paint;
 import android.os.Bundle;
 import android.view.View;
 import android.view.View.OnClickListener;
@@ -27,10 +29,12 @@ public class TowerDefenseActivity extends Activity implements OnClickListener {
     	
     	 if (v == button_start)
     	 {
-    		 new AlertDialog.Builder(this).setMessage(
-                     "hallo").setNeutralButton(
-                     "ok",
-                     null).show();
+    		
+    		 Spielfeld d = new Spielfeld(this);
+    		 setContentView(d);
+    		 
+    		
+    		 
     	 }
     	
     	
