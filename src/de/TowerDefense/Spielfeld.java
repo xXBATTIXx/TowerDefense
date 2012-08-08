3e4fa5ce469f9f7eadd298af5aed7a8f4618f190
Heinz <BATTIHeinz@web.de>
2012-08-08 20:54:38 +0200

    ka

--- src/de/TowerDefense/Spielfeld.java
+++ src/de/TowerDefense/Spielfeld.java
@@ -1,8 +1,48 @@
 package de.TowerDefense;
 
-public class Spielfeld {
+<<<<<<< HEAD
+import android.graphics.Canvas;
+
+public class Spielfeld
+{
+	public Canvas canvas;
+=======
+import android.content.Context;
+import android.graphics.Canvas;
+import android.graphics.Color;
+import android.graphics.Paint;
+import android.view.View;
+
+public class Spielfeld extends View {
 	
+>>>>>>> origin/CherryCola
 	private int x;
 	private int y;
+	
+	
+	
+	 public Spielfeld(Context context) {
+         super(context);
+     }
+ 
+     @Override
+     protected void onDraw(Canvas c){
+         super.onDraw(c);
+         
+         Paint paint = new Paint();
+		 paint.setStyle(Paint.Style.FILL);
 
+		 // make the entire canvas white
+		 paint.setColor(Color.WHITE);
+		 c.drawPaint(paint);
+		 
+		 paint.setColor(Color.BLUE);
+		 c.drawRect(50, 100, 200, 3, paint);
+         
+     }
+
+	
+
+	int a =5;
+	
 }
